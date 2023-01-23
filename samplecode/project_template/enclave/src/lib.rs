930865a2be9ed90805f700a44e9ea165187c9f30
Nathan Prat <nathan.prat@gmail.com>
2023-01-23 11:58:07 +0100

    cleanup 2

--- samplecode/project_template/enclave/src/lib.rs
+++ samplecode/project_template/enclave/src/lib.rs
@@ -29,10 +29,37 @@ use sgx_types::*;
 use std::io::{self, Write};
 use std::slice;
 
+// TODO? Ideally we want to run some basic tests, but it would require more work:
+// - AT LEAST: add some missing "import" in Enclave.edl
+// - resolve "undefined reference" errors for each of those
+// - FIX runtime error: [-] ECALL Enclave Failed SGX_ERROR_STACK_OVERRUN!
+fn test_lib() {
+    // This WOULD FAIL, cf docstring if this fn
+    // let response = http_grpc_client::sp_offchain_fetch_from_remote_grpc_web(
+    //     None,
+    //     "https://www.google.com",
+    //     &http_grpc_client::RequestMethod::Get,
+    //     None,
+    //     core::time::Duration::from_millis(1000),
+    // )
+    // .unwrap();
+
+    // let response = http_grpc_client::http_req_fetch_from_remote_grpc_web(
+    //     None,
+    //     "http://postman-echo.com/get?hello=world",
+    //     &http_grpc_client::RequestMethod::Get,
+    //     None,
+    //     core::time::Duration::from_millis(1000),
+    // )
+    // .unwrap();
+}
+
 #[no_mangle]
 pub extern "C" fn ecall_test(some_string: *const u8, some_len: usize) -> sgx_status_t {
     let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
     let _ = io::stdout().write(str_slice);
+
+    test_lib();
 
     println!("Message from the enclave");
 
